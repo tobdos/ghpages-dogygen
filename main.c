8461bc3126bd5773d8ccd4efbe4a64c3a89c81f2
tobdos <103242050+tobdos@users.noreply.github.com>
2022-04-13 14:06:35 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,35 @@
+/**
+ * @file main.c
+ * @author maiestef2
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-08
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdio.h>
+
+/** 
+ * \brief calculates the sum of the two parameters and adds one. 
+ * \param [in] a first operand
+ * \param [in] b second operand
+ * 
+ * \return sum plus one 
+ */
+int sum(int a, int b)
+{
+    return a + b + 1;
+}
+
+/** 
+ * \brief main function of my project 
+ * A long version of the desciption 
+*/
+int main()
+{
+    printf("Hello World!\n");
+    printf("Hello Tobias!\n");
+    return 0;
+}
