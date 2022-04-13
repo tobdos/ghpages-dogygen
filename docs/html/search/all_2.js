665d7db14c14692a518063cb1fe1239e48e69365
GitHub Action <action@github.com>
2022-04-13 13:59:12 +0000

    Add changes

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['testmarkdown2_2emd_0',['testmarkdown2.md',['../testmarkdown2_8md.html',1,'']]],
-  ['this_20is_20the_20mainpage_20in_20markdown_1',['This is the mainpage in markdown',['../md_src_mainpage.html',1,'']]]
+  ['this_20is_20the_20mainpage_20in_20markdown_1',['This is the mainpage in markdown',['../md_docs_mainpage.html',1,'']]]
 ];
--- docs/html/search/pages_1.js
+++ docs/html/search/pages_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['this_20is_20the_20mainpage_20in_20markdown_0',['This is the mainpage in markdown',['../md_src_mainpage.html',1,'']]]
+  ['this_20is_20the_20mainpage_20in_20markdown_0',['This is the mainpage in markdown',['../md_docs_mainpage.html',1,'']]]
 ];
