80630390628cc2d611c9a05506de4f675f3ca2c3
tobdos <103242050+tobdos@users.noreply.github.com>
2022-04-13 14:30:12 +0200

    Delete docs/html directory

--- docs/html/search/files_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['sourcetest_2ecpp_0',['sourceTest.cpp',['../sourceTest_8cpp.html',1,'']]]
-];
--- docs/html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['sum_0',['sum',['../main_8c.html#a5ceb2172cbc407c45e7e9f2d22a2b5c1',1,'main.c']]]
-];
