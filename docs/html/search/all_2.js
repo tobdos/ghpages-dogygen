9369cd20c064835f3d0a424227b71736a0781eca
GitHub Action <action@github.com>
2022-04-13 13:11:19 +0000

    Add changes

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['testmarkdown_2emd_0',['testmarkdown.md',['../testmarkdown_8md.html',1,'']]],
-  ['testmarkdown2_1',['testmarkdown2',['../md_src_testmarkdown2.html',1,'']]],
-  ['testmarkdown2_2emd_2',['testmarkdown2.md',['../testmarkdown2_8md.html',1,'']]]
+  ['testmarkdown2_2emd_1',['testmarkdown2.md',['../testmarkdown2_8md.html',1,'']]]
 ];
--- docs/html/search/pages_0.js
+++ docs/html/search/pages_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['mermaid_20testing_0',['Mermaid Testing',['../index.html',1,'']]]
+  ['mermaid_20testing_0',['Mermaid Testing',['../index.html',1,'']]],
+  ['mermaid_20testing_202_1',['Mermaid Testing 2',['../md_src_testmarkdown2.html',1,'']]]
 ];
