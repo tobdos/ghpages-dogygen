939879c6b11d151c21d2538c5a0b1b7f736f84d2
tobdos <103242050+tobdos@users.noreply.github.com>
2022-04-14 14:34:47 +0200

    Update sourceTest.cpp

--- src/sourceTest.cpp
+++ src/sourceTest.cpp
@@ -8,11 +8,28 @@
  * @copyright Copyright (c) 2022
  * 
  */
-
 #include <iostream>
+
+/**
+ * \brief Start brief with capital letter and end with dot.
+ * \details Use explicit \details instead of leaving a blank line after \\brief.
+ *          Other files.c or functions() will be linked by just writing their
+ *          plain name, however #defines must be prefixed with \ref in order to
+ *          get hyperlinked.
+ * \pre The \pre command is useful for documenting _what_ must be done
+ *      before this function _may_ be called.
+ * \param[out] parm1 Description for output parameter
+ * \param parm2 No [in] on input parameters
+ * \param parm3 This one somehow relates to \p parm1
+ * \return Result of computation
+ */
+int doxygen_example(){
+    std::cout << " This is a doxygen example function";
+}
+
 /** 
  * \brief main function
- * A long description of the main function
+ * \details A long description of the main function.
  */
 int main() {
     std::cout << "Hello World!";
