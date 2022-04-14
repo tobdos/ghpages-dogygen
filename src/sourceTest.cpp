f27fe8e93b7c4fe9759e57c4290957376c7570cb
tobdos <103242050+tobdos@users.noreply.github.com>
2022-04-14 08:08:38 +0200

    Update sourceTest.cpp

--- src/sourceTest.cpp
+++ src/sourceTest.cpp
@@ -1,11 +1,20 @@
-// Your First C++ Program
+/**
+ * @file main.c
+ * @author dosetobi
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-14
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 
 #include <iostream>
-/*
-multiline comment test
-test12
-test3
-*/
+/**
+ * multiline comment test
+ * test12
+ * test3
+ */
 int main() {
     std::cout << "Hello World!";
     return 0;
